f663cebde7463806ab972f5871ce03006eda826c
Christian Toffolo <ian@webian.it>
2020-11-01 19:44:26 +0100

    [TODO][TASK] Customize FrontendEditInitiator middleware

--- Classes/Middleware/FrontendEditInitiator.php
+++ Classes/Middleware/FrontendEditInitiator.php
@@ -22,8 +22,7 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Backend\FrontendBackendUserAuthentication;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Feedit\DataHandling\FrontendEditDataHandler;
-use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use Webian\Visualedit\DataHandling\FrontendEditDataHandler;
 
 /**
  * PSR-15 middleware initializing frontend editing
@@ -43,62 +42,17 @@ class FrontendEditInitiator implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if (isset($GLOBALS['BE_USER']) && $GLOBALS['BE_USER'] instanceof FrontendBackendUserAuthentication) {
-            $config = $GLOBALS['BE_USER']->getTSConfig()['admPanel.'] ?? [];
-            $active = (int)$GLOBALS['TSFE']->displayEditIcons === 1 || (int)$GLOBALS['TSFE']->displayFieldEditIcons === 1;
-            // Include classes for editing IF editing module in Admin Panel is open
-            if ($active && isset($config['enable.'])) {
-                foreach ($config['enable.'] as $value) {
-                    if ($value) {
-                        $parameters = $request->getParsedBody()['TSFE_EDIT'] ?? $request->getQueryParams()['TSFE_EDIT'] ?? null;
-                        $isValidEditAction = $this->isValidEditAction($parameters);
-                        if ($GLOBALS['TSFE'] instanceof TypoScriptFrontendController) {
-                            // Grab the Page TSConfig property that determines which controller to use.
-                            $pageTSConfig = $GLOBALS['TSFE']->getPagesTSconfig();
-                            $controllerKey = $pageTSConfig['TSFE.']['frontendEditingController'] ?? 'default';
-                        } else {
-                            $controllerKey = 'default';
-                        }
-                        /** @deprecated will be removed in TYPO3 v10.0. */
-                        $controllerClassName = $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tsfebeuserauth.php']['frontendEditingController'][$controllerKey] ?? \TYPO3\CMS\Core\FrontendEditing\FrontendEditingController::class;
-                        if (!empty($controllerClassName)) {
-                            /** @deprecated will be removed in TYPO3 v10.0. */
-                            $GLOBALS['BE_USER']->frontendEdit = GeneralUtility::makeInstance(
-                                $controllerClassName,
-                                $parameters
-                            );
-                        }
-                        if ($isValidEditAction) {
-                            GeneralUtility::makeInstance(FrontendEditDataHandler::class, $parameters)->editAction();
-                        }
-                        break;
-                    }
-                }
-            }
+        // Process only if valid BE user has access to Visualedit module
+        if (
+            isset($GLOBALS['BE_USER']) &&
+            $GLOBALS['BE_USER'] instanceof FrontendBackendUserAuthentication &&
+            $GLOBALS['BE_USER']->check('modules', 'web_visualedit')
+        ) {
+            $GLOBALS['TSFE']->displayEditIcons = '1';
+            $GLOBALS['TSFE']->displayFieldEditIcons = '1';
+            $GLOBALS['TSFE']->set_no_cache('EXT:Visualedit - Frontend editing', true);
+            GeneralUtility::makeInstance(FrontendEditDataHandler::class)->editAction();
         }
         return $handler->handle($request);
-    }
-
-    /**
-     * Returns TRUE if an edit-action is sent from the Admin Panel
-     *
-     * @param array|null $parameters
-     * @return bool
-     */
-    protected function isValidEditAction(array &$parameters = null): bool
-    {
-        if (!is_array($parameters)) {
-            return false;
-        }
-        if ($parameters['cancel']) {
-            unset($parameters['cmd']);
-        } else {
-            $cmd = (string)$parameters['cmd'];
-            if (($cmd !== 'edit' || is_array($parameters['data']) && ($parameters['doSave'] || $parameters['update'] || $parameters['update_close'])) && $cmd !== 'new') {
-                // $cmd can be a command like "hide" or "move". If $cmd is "edit" or "new" it's an indication to show the formfields. But if data is sent with update-flag then $cmd = edit is accepted because edit may be sent because of .keepGoing flag.
-                return true;
-            }
-        }
-        return false;
     }
 }
